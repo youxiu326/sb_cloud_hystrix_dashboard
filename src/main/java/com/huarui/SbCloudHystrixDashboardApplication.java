fe71ae3fbd1fc13a5136a0969a26ca03e37f2bb4
youxiu326 <youxiu326@163.com>
2019-03-17 10:55:37 +0800

    Initial commit

---
+++ src/main/java/com/huarui/SbCloudHystrixDashboardApplication.java
@@ -0,0 +1,15 @@
+package com.huarui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class SbCloudHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbCloudHystrixDashboardApplication.class, args);
+	}
+
+}
